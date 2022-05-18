ca6b7c5dd67e0e13748ec17de6cc9d95fc3d8e6c
VergaJU <jacopoumberto.verga@gmail.com>
2022-05-18 16:20:24 +0100

    Create seurat_to_h5ad.sh
    add bash script to convert seurat objects to h5ad file format

---
+++ Merge_bath-correct/scripts/seurat_to_h5ad.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+file=$1 # input file
+
+mkdir temp # create temporary directory
+
+~/Documents/PROJECT/scripts_env/scripts/seurat_to_mtx.R $file  # convert in matrix
+
+mv ./temp/genes.tsv ./temp/old.tsv # mv genes in a new file
+
+# add the genes in a new genes.tsv file with 2 columns
+
+for f in $(cat ./temp/old.tsv);do
+    echo -e "$f\t$f" >> ./temp/genes.tsv
+    done
+
+#convert in a h5ad file
+~/Documents/PROJECT/scripts_env/scripts/mtx_to_h5ad.py $file
+
+# remove temp directory
+rm -fr temp/
