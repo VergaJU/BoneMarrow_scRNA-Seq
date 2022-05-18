52e17d6fb4890e0d516957da499e87bb016e21ce
VergaJU <jacopoumberto.verga@gmail.com>
2022-05-18 11:05:51 +0100

    Update seurat_to_mtx.R
    add TODO for genes.tsv file

--- Merge_bath-correct/scripts/seurat_to_mtx.R
+++ Merge_bath-correct/scripts/seurat_to_mtx.R
@@ -9,12 +9,12 @@ args <- commandArgs(trailingOnly = T) # save argument in a vector
 #Note that only Gene Expression is exported
 
 Seurat_to_MM10X <- function(Seurat,prefix){
-writeMM(Seurat@assays$RNA@counts,paste(prefix,"matrix.mtx",sep="")) # Write matrix w/ counts
-write(x = rownames(Seurat@assays$RNA@counts),file = paste(prefix,"genes.tsv",sep="")) # write gene
-## NOTE: genes should have 2 columns, TO BE FIXED
-write(x = colnames(Seurat@assays$RNA@counts),file = paste(prefix,"barcodes.tsv",sep="")) # write barcodes
-write.table(x = Seurat@meta.data, file = paste(prefix,"metadata.tsv",sep=""), # write metadata
-quote=FALSE,sep="\t")
+    writeMM(Seurat@assays$RNA@counts,paste(prefix,"matrix.mtx",sep="")) # Write matrix w/ counts
+    write(x = rownames(Seurat@assays$RNA@counts),file = paste(prefix,"genes.tsv",sep="")) # write gene
+    ## TODO: genes should have 2 columns, TO BE FIXED
+    write(x = colnames(Seurat@assays$RNA@counts),file = paste(prefix,"barcodes.tsv",sep="")) # write barcodes
+    write.table(x = Seurat@meta.data, file = paste(prefix,"metadata.tsv",sep=""), # write metadata
+    quote=FALSE,sep="\t")
 }
 
 dat <- readRDS(args[1]) # Read input file
