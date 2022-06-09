faad80cd9296dbbd6c254d84b5a3aaefca5da738
VergaJU <jacopoumberto.verga@gmail.com>
2022-06-09 11:30:00 +0100

    some updates to made the scripts working inside the container

---
+++ Pre-process/container/scripts/filter_empty_v2.R
@@ -0,0 +1,77 @@
+#! /home/jacopo/miniconda3/envs/pre_process/bin/Rscript
+
+library(tidyverse, quietly=T) # packages for data wrangling, visualization etc
+library(DropletUtils, quietly=T)
+library(scDblFinder, quietly=T)
+library(Matrix, quietly=T)
+library(tools, quietly = T)
+library(R.utils, quietly = T)
+
+set.seed(1) # set seed to increase reproducibility
+#setwd("~/Documents/PROJECT/MM/datasets/SRA_data/PRJNA694128/25183/SAMN17249432/kb_out/counts_unfiltered/")
+
+
+# save arguments in a vector
+args <- commandArgs(trailingOnly = T) 
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied\n 
+       First argument: `path/to/files/`\n 
+       Second argument (optional): FDR threshold ", call.=FALSE)
+} else if (length(args)==1) {
+  # default select FDR threshold of 0.1 file
+  print("No FDR threshold supplied, default threshold of 0.1 selected")
+  args[2] <- 0.1
+}
+
+filepath <- args[1]
+files <- list.files(filepath)
+
+if (file.exists(paste0(filepath, "barcodes.tsv.gz"))) {
+  cat("\nFiles compressed, gunzipping...")
+  for (f in files) {
+    gunzip(paste0(filepath, f), remove = T)
+    cat("...")
+  }
+  files <- list.files(filepath)
+  cat("\nFiles decompressed, checking file names...")
+} else {
+  cat("\nFiles not compressed, checking file names...")
+}
+
+
+if ("genes.tsv" %in% files){
+  genes.file <- "genes.tsv"
+} else {
+  genes.file <- "features.tsv"
+}
+
+cat("\nFilenames checked\nStart filtering empty drops with a threshold of", args[2],
+    "\nThe process can take some minutes\n")
+
+m <- Matrix::readMM(paste0(filepath, "/matrix.mtx")) # load matrix
+genes  <- read.csv(paste0(filepath, genes.file), sep = "\t", header = F)# attach genes
+rownames(m) <- genes[,2]
+colnames(m) <- read.csv(paste0(filepath, "/barcodes.tsv"), sep = "\t", header = F)[,1] # attach barcodes
+empty <- emptyDrops(m) # compute the probability of empty droplets
+full <- empty$FDR <= args[2] # filter by selected FDR threshold
+full[is.na(full)] <- FALSE # remove NAs
+filtered_m <- m[,full]  # subset original matrix
+
+cat("Empty droplets filtered correctly \n",
+    "Starting filtering of doublets using scDblFinder\n")
+
+dat <- SingleCellExperiment(assay = list(counts = filtered_m))
+dat <- scDblFinder(dat)
+
+singlets <- as.logical(as.integer(dat$scDblFinder.class) == 1)
+filtered_m1 <- filtered_m[,singlets]  # subset original matrix
+dim(m)
+dim(filtered_m)
+dim(filtered_m1)
+
+
+DropletUtils::write10xCounts("./kb_out/counts_filtered", gene.symbol = genes[,2], filtered_m1, overwrite = T) # write result
+
+cat("Filtering completed correctly.\n")
---
+++ Pre-process/container/scripts/functions.sh
@@ -0,0 +1,199 @@
+#! /usr/bin/bash
+
+
+
+# Check if the folder already exist, if not create it
+# First argument: str (patient or sample)
+# Second argument: variable with patient or sample ID
+# e.g. create_folder "patient" 51449
+# e.g. create_folder "sample" SAMN17249432
+
+# create folder for the patient and samples (from first 2 columns of the csv input file)
+## input:
+# 1- string: patient or sample
+# 2- string: entry (first or second column input csv)
+
+## output
+# working directory for the patient/sample
+create_folder () {
+    folders=($(ls))
+    if [[ "${folders[@]}" =~ "${2}" ]]
+    then
+        printf "Entering the ${1} ${2} folder\n"
+        cd ${2}
+    else
+        printf "Creating and entering the ${1} ${2} folder\n"
+        mkdir ${2}
+        cd ${2}
+    fi
+}
+
+
+# check if the csv file contains links to download bam files or SRA entries
+## input: 3rd column of the csv file, SRA entry or url to download bam file
+## output: variable "filetype" for next steps
+check_filetype () {
+    if [[ "${1}" =~ "https" ]]
+    then
+        filetype="BAM"
+    elif [[ "${1}" =~ "SRR" ]]
+    then
+        filetype="SRA"
+    fi
+}
+
+
+# DOWNLOAD THE BAM FILE
+## input: url (${url})
+## output: bam file
+download_bam () {
+    printf "Downloading bam file...\n"
+    wget -c ${1}
+    filename=$(ls) # update filename
+}
+
+# Download and convert SRA file with fasterq-dump
+## input: SRR entry
+## output: 3 fastq files (R1, R2, R3)
+download_sra () {
+    printf "Downloading SRA file..\n";
+    fasterq-dump ${1} -o ${1} -O fastq/ -S --include-technical |& tee -a fasterq-dump.LOG
+    cache-mgr -c ~/.sratoolkit.2.11.3-ubuntu64/cache/
+
+}
+
+# Run cellranger to convert the bam file to fastq files, as input it takes the filename 
+## Input: string, filename, bam file previously downloaded
+## Output: 3 fastq files
+
+cellranger () {
+    printf "running CellRanger bamtofastq to obtain the fastq files\n"
+    ## Use cellranger to get the fastq files
+    ~/.cellranger-6.1.2/lib/bin/bamtofastq \
+        --nthreads=6 \
+        --reads-per-fastq=100000000000 \
+        --traceback \
+        ${filename} \
+        ./fastq/ \
+        |& tee -a bamtofastq.LOG
+}
+
+# Check if cellranger converted correctly the files
+# Move the files in the fastqs/ folder
+# Remove the other folders and bam files
+
+check_bam () {
+    # Control the log file and reformat the output for forwarding analyses
+    if [[ "$(tail -n1 bamtofastq.LOG)" =~ "Writing finished" ]]
+    then
+        printf "bamtofastq conversion finished correctly \nRemoving bam files and moving fastq files\n"
+        fastqfiles=($(find ./fastq/ | grep bamto)) # store fastqfiles names and path
+        for file in ${fastqfiles[@]};do
+            suffix=$(echo ${file} | awk -F "/" '{print  $NF}' | cut -d "_" -f 2-) # save the suffix name
+            mv ${file} ./fastq/$name"_"$suffix # rename the files with the name given in the csv file 
+                                               # and move in fastq directory
+        done
+        rm -fr ./fastq/*/* # remove empty folders
+        rm -f *.bam* # remove bam file
+    else
+        printf "ERROR: bamtofastq conversion failed, EXITING.\n"
+        exit 1
+    fi
+}
+
+check_sra () {
+    # control fasterq-dump output and reformat the output for forwarding analyses
+    if [[ "$(tail -n1 fasterq-dump.LOG)" =~ "reads written" ]]
+    then
+        printf "SRA file converted correctly\n"
+        # rename fastq files in R1, R2 anr I1
+        for file in ./fastq/* ; do
+        len="$(cat ${file} | head -80 | sed -n 2~4p | awk '{ print length }' | awk -F : '{sum+=$1} END {print sum/NR}' | cut -f1 -d".")";
+            if [[ $len -lt 26 ]]; then
+            mv $file ${file%_*.fastq}_I1.fastq
+            elif [[ $len -eq 26 || $len -eq 28 ]]; then
+            mv $file ${file%_*.fastq}_R1.fastq
+            elif [[ $len -gt 26 && $len -gt 28 ]]; then
+            mv $file ${file%_*.fastq}_R2.fastq           
+            fi
+        done
+
+    else
+        print "ERROR: File not downloaded correctly, EXITING\n"
+        exit 1
+    fi
+}
+
+# Determine the technology used to exert the scRNA-Seq experiment. Now it recognize only 10x genomics experiments
+# No arguments needed
+# Not used now, technology inputted in the csv file
+# TODO: FIX function for different technologies
+determine_tech () {
+    printf "Checking technology\n"
+
+    fastqs=($(find . | grep _R[1-2] | sort)) # find fastq files of interest
+
+    #echo ${fastqs[@]}
+
+    # detect chemistry version by checking the length of the first 20 R1s and taking the floor of the average
+    r1_len="$(cat fastq/*1* | head -80 | sed -n 2~4p | awk '{ print length }' | awk -F : '{sum+=$1} END {print sum/NR}' | cut -f1 -d".")"
+    if [[ $r1_len =~ 28 ]]
+    then
+        tech="10xv3"
+    elif [[ $r1_len =~ 26 ]]
+    then
+        tech="10xv2"
+    else
+    echo 'Unable to determine version\n'
+    fi
+}
+
+
+# Get the counts using the kb wrapper for kallisto|bustools.
+# Firstly activate the conda evironment, the environment need to bre created using the pre_process.yml file
+# inputs: fastq files, index, t2g, technology
+# output: count matrices, unfiltered/filtered
+get_counts () {
+    #activate conda env
+    conda activate pre_process
+
+    fastqs=($(find . | grep _R[1-2] | sort))  # find fastq files of interest
+
+    # run kallisto|bustools
+    printf "running kb to obtain counts\n"
+    kb count \
+        --verbose \
+        -t 6 \ # number of threads
+        --cellranger \ # cellranger output
+        -i $index \ # index
+        -g $t2g \ # transcript to gene
+        -x $tech \ # technology
+        -o ./kb_out \ # output folder
+        ${fastqs[@]} \ # fastq files (R1, R2)
+        --overwrite \
+        |& tee -a kallisto.LOG 
+
+    if [[ "$(find kb_out) " =~ "cellranger" ]] # check if counts obtained correctly
+    then
+        # filter empty droplets using EmptyDroplets, FDR<0.1
+        # filter doublets using scDblFinder
+        # output: folder with filtered cells
+        printf "filtering empty droplets with FDR threshold of 0.1\n"
+        /scripts/filter_empty_v2.R ./kb_out/counts_unfiltered/cellranger/ 0.1 \
+        |& tee -a filter_empty.LOG
+    else
+        printf "ERROR: kallisto|bustool failed obtaining the counts. EXITING"
+        exit 1
+    fi
+
+    if [[ "$(find kb_out) " =~ "counts_filtered" ]]
+    then
+        # remove fastq and bus files
+        printf "Files preprocessed correctly\nRemove fastq and bus files to save hard drive space\nReturn in root folder\n"
+        find . -name "*.fastq" -delete
+        find . -name "*.bus" -delete
+    else
+        printf "ERROR: Cells not correctly filtered. EXITING"
+        exit 1
+    fi
+}
