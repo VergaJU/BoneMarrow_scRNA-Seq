385bb7dd3b5b936bfc010b9ffe61bc6a9be19765
VergaJU <jacopoumberto.verga@gmail.com>
2022-06-13 19:57:22 +0100

    Update swarms.py

--- Merge_bath-correct/swarm_particles/swarms.py
+++ Merge_bath-correct/swarm_particles/swarms.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 import scanpy as sc
-import pyswarms as ps
-import scib
+import sys
+#import pyswarms as ps
+#import scib
 
+inputfile = sys.argv[1]
+outputfile = inputfile[:-5] + "_clustered.h5ad"
 
 # Read file
-adata = sc.read("./SMM.h5ad")
+adata = sc.read(inputfile)
 
 
 # filter genes expressed in less than 3 cells
@@ -16,6 +19,16 @@ sc.pp.filter_genes(adata, min_cells = 3)
 # normalize data
 sc.pp.normalize_total(adata, target_sum=1e4)
 
+# Run UMAP and clustering as reference
+sc.pp.log1p(adata)
+sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+sc.tl.pca(adata, svd_solver='arpack')
+sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)
+sc.tl.umap(adata)
+sc.tl.leiden(adata)
+adata.write(outputfile)
+
+# new matrix used to be modified by birds
 adata_norm = adata.X
 
 # convert to dense matrix
@@ -36,5 +49,12 @@ new_adata.var = adata.var # append metadata
 new_adata.obs = adata.obs # append metadata
 
 
-## UMAP and clustering for both the files
+## UMAP and clustering of the new anndata
 
+sc.pp.log1p(new_adata)
+sc.pp.highly_variable_genes(new_adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+sc.tl.pca(new_adata, svd_solver='arpack')
+sc.pp.neighbors(new_adata, n_neighbors=10, n_pcs=40)
+sc.tl.umap(new_adata)
+sc.tl.leiden(new_adata)
+new_adata.write(outputfile)
