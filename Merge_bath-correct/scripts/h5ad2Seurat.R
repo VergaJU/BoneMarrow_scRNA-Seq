fcfef8fa1d9d5f0656fc5453ffb2403dd6d6254c
VergaJU <jacopoumberto.verga@gmail.com>
2022-06-15 15:42:04 +0100

    Create h5ad2Seurat.R

---
+++ Merge_bath-correct/scripts/h5ad2Seurat.R
@@ -0,0 +1,42 @@
+#!/home/jacopo/anaconda3/envs/convert/bin/Rscript
+library(reticulate)
+library(Seurat)
+library(Matrix)
+library(optparse)
+
+option_list = list(
+  make_option(
+    c("-i", "--input_file"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = 'Path to input file.'
+  ),
+  make_option(
+    c("-o", "--output_file"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = 'Path to output file.'
+  )
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+
+#make sure you have python installed. This works with Python 3.8.5
+#if you don't already have it, install the python "scanpy" package using reticulate use the following to install it:
+#py_install("scanpy")
+
+sc <- import("scanpy")
+
+atlas.data <- sc$read_h5ad(opt$input_file)
+
+counts <- t(adata$X)
+colnames(counts) <-  adata$obs_names$to_list()
+rownames(counts) <-  adata$var_names$to_list()
+
+seurat <- CreateSeuratObject(counts)
+seurat <- AddMetaData(seurat,  adata$obs)
+
+saveRDS(seurat, file = opt$output_file)
