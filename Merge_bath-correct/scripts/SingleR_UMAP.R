8ca9d286d9483f4182ad293c275263f753a30fb1
VergaJU <jacopoumberto.verga@gmail.com>
2022-06-16 19:44:30 +0100

    Create SingleR_UMAP.R

---
+++ Merge_bath-correct/scripts/SingleR_UMAP.R
@@ -0,0 +1,22 @@
+library(Seurat)
+library(SingleCellExperiment)
+library(SingleR)
+
+dat <- readRDS(opt$input_file)
+ref <- readRDS(opt$ref)
+
+dat <- as.SingleCellExperiment(dat)
+ref <- as.SingleCellExperiment(ref)
+
+# run SingleR
+pred <- SingleR(test=dat, ref=ref, labels=ref[['celltype']], de.method="wilcox", assay.type.test = 'logcounts', assay.type.ref = 'logcounts')
+
+df <- data.frame(label = pred$labels)
+rownames(df) <- rownames(pred)
+
+# save results
+pred <- as
+saveRDS(pred, file = paste0(opt$output_name, ".Rds")
+write.table(df, file = paste0(opt$output_name, '_cell_labels.csv'), sep = '\t', row.names = T, col.names = T, quote = F)
+
+##TODO: add UMAP representation of celltypes
