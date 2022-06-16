96f73800c263c5d2edb99925d78899067823af32
VergaJU <jacopoumberto.verga@gmail.com>
2022-06-16 18:05:26 +0100

    correct matrix
    work only with some

--- Merge_bath-correct/scripts/h5ad2Seurat.R
+++ Merge_bath-correct/scripts/h5ad2Seurat.R
@@ -27,6 +27,7 @@ opt <- parse_args(OptionParser(option_list=option_list))
 #make sure you have python installed. This works with Python 3.8.5
 #if you don't already have it, install the python "scanpy" package using reticulate use the following to install it:
 #py_install("scanpy")
+## method find here https://github.com/satijalab/seurat/issues/4711
 
 sc <- import("scanpy")
 
@@ -35,6 +36,8 @@ adata <- sc$read_h5ad(opt$input_file)
 counts <- t(adata$X)
 colnames(counts) <-  adata$obs_names$to_list()
 rownames(counts) <-  adata$var_names$to_list()
+counts <- Matrix::Matrix(as.matrix(counts), sparse = T)
+#seurat <- as(counts, "dgCMatrix")
 
 seurat <- CreateSeuratObject(counts)
 seurat <- AddMetaData(seurat,  adata$obs)
