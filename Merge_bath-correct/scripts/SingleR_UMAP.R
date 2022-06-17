cb2afb88b4f0318da4a290aaac0ce8f5f015c662
VergaJU <jacopoumberto.verga@gmail.com>
2022-06-17 10:32:06 +0100

    keep seurat obj, add labels, fix file names

--- Merge_bath-correct/scripts/SingleR_UMAP.R
+++ Merge_bath-correct/scripts/SingleR_UMAP.R
@@ -1,29 +1,31 @@
 library(Seurat)
 library(SingleCellExperiment)
 library(SingleR)
+library(stringr)
 
 dat <- readRDS(opt$input_file)
 ref <- readRDS(opt$ref)
 
-dat <- as.SingleCellExperiment(dat)
+dat1 <- as.SingleCellExperiment(dat)
 ref <- as.SingleCellExperiment(ref)
 
 # run SingleR
-pred <- SingleR(test=dat, ref=ref, labels=ref[['celltype']], de.method="wilcox", assay.type.test = 'logcounts', assay.type.ref = 'logcounts')
+pred <- SingleR(test=dat1, ref=ref, labels=ref[['celltype']], de.method="wilcox", assay.type.test = 'logcounts', assay.type.ref = 'logcounts')
 
 df <- data.frame(label = pred$labels)
 rownames(df) <- rownames(pred)
 
 # save results
-pred <- as.Seurat(pred)
 
+dat <- AddMetaData(dat, df)
 
-plot <- DimPlot(pred, reduction="umap",group.by="celltype", label=T, pt.size=.1,raster=F)
+plot <- DimPlot(dat, reduction="umap",group.by="label", label=T, pt.size=.1,raster=F)
 plot <- plot + NoLegend()
-svg("UMAP_batch_harmony.svg")
+svg(str_replace(input_file, ".Rds", "UMAP_label.svg"))
 print(plot)
 dev.off()
 
 
-saveRDS(pred, file = paste0(opt$output_name, ".Rds"))
-write.table(df, file = paste0(opt$output_name, '_cell_labels.csv'), sep = '\t', row.names = T, col.names = T, quote = F)
+saveRDS(pred, file = str_replace(input_file, ".Rds", "_singleR_results.Rds"))
+saveRDS(dat, file = str_replace(input_file, ".Rds", "_labelled.Rds"))
+write.table(df, file =str_replace(input_file, ".Rds", '_cell_labels.csv'), sep = '\t', row.names = T, col.names = T, quote = F)
