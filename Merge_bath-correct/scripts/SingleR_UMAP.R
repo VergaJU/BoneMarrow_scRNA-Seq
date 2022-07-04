34dee4b8eb43e240e726122666f14dd8eaf5ea5e
VergaJU <jacopoumberto.verga@gmail.com>
2022-07-04 18:28:12 +0100

    add parallelization, fixed input variables

--- Merge_bath-correct/scripts/SingleR_UMAP.R
+++ Merge_bath-correct/scripts/SingleR_UMAP.R
@@ -3,15 +3,21 @@ library(Seurat)
 library(SingleCellExperiment)
 library(SingleR)
 library(stringr)
+library(BiocParallel)
 
-dat <- readRDS(commandArgs(trailingOnly = TRUE)[1])
-ref <- readRDS(commandArgs(trailingOnly = TRUE)[2])
 
-#dat1 <- as.SingleCellExperiment(dat)
+multicoreParam <- MulticoreParam(workers = 6)
+input_file <- commandArgs(trailingOnly = TRUE)[1]
+reference <- commandArgs(trailingOnly = TRUE)[2]
+
+dat <- readRDS(input_file)
+ref <- readRDS(reference)
+
+dat1 <- as.SingleCellExperiment(dat)
 #ref <- as.SingleCellExperiment(ref)
 
 # run SingleR
-pred <- SingleR(test=dat, ref=ref, labels=ref[['celltype']], de.method="wilcox", assay.type.test = 'logcounts', assay.type.ref = 'logcounts')
+pred <- SingleR(test=dat, ref=ref, labels=ref[['celltype']], de.method="wilcox", assay.type.test = 'logcounts', assay.type.ref = 'logcounts', BPPARAM=multicoreParam)
 
 df <- data.frame(label = pred$labels)
 rownames(df) <- rownames(pred)
@@ -20,13 +26,13 @@ rownames(df) <- rownames(pred)
 
 dat <- AddMetaData(dat, df)
 
-plot <- DimPlot(dat, reduction="umap",group.by="label", label=T, pt.size=.1,raster=T)
-plot <- plot + NoLegend()
-png(str_replace(dat, ".Rds", "_UMAP_label.png"))
-print(plot)
-dev.off()
+#plot <- DimPlot(dat, reduction="umap",group.by="label", label=T, pt.size=.1,raster=T)
+#plot <- plot + NoLegend()
+#png(str_replace(dat, ".Rds", "_UMAP_label.png"))
+#print(plot)
+#dev.off()
 
 
-saveRDS(pred, file = str_replace(dat, ".Rds", "_singleR_results.Rds"))
-saveRDS(dat, file = str_replace(dat, ".Rds", "_labelled.Rds"))
-write.table(df, file =str_replace(dat, ".Rds", '_cell_labels.csv'), sep = '\t', row.names = T, col.names = T, quote = F)
+saveRDS(pred, file = str_replace(input_file, ".Rds", "_singleR_results.Rds"))
+saveRDS(dat, file = str_replace(input_file, ".Rds", "_labelled.Rds"))
+write.table(df, file =str_replace(input_file, ".Rds", '_cell_labels.csv'), sep = '\t', row.names = T, col.names = T, quote = F)
