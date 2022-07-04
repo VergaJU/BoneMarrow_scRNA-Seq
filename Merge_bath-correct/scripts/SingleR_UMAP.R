23b911c2e5a62e69d9f414f1fc1c1b643178ad34
VergaJU <jacopoumberto.verga@gmail.com>
2022-07-04 12:07:39 +0100

    fixed variables
    input= sce objects, if seurat un-hash lines 9 and 10

--- Merge_bath-correct/scripts/SingleR_UMAP.R
+++ Merge_bath-correct/scripts/SingleR_UMAP.R
@@ -6,11 +6,11 @@ library(stringr)
 dat <- readRDS(commandArgs(trailingOnly = TRUE)[1])
 ref <- readRDS(commandArgs(trailingOnly = TRUE)[1])
 
-dat1 <- as.SingleCellExperiment(dat)
-ref <- as.SingleCellExperiment(ref)
+#dat1 <- as.SingleCellExperiment(dat)
+#ref <- as.SingleCellExperiment(ref)
 
 # run SingleR
-pred <- SingleR(test=dat1, ref=ref, labels=ref[['celltype']], de.method="wilcox", assay.type.test = 'logcounts', assay.type.ref = 'logcounts')
+pred <- SingleR(test=dat, ref=ref, labels=ref[['celltype']], de.method="wilcox", assay.type.test = 'logcounts', assay.type.ref = 'logcounts')
 
 df <- data.frame(label = pred$labels)
 rownames(df) <- rownames(pred)
@@ -21,11 +21,11 @@ dat <- AddMetaData(dat, df)
 
 plot <- DimPlot(dat, reduction="umap",group.by="label", label=T, pt.size=.1,raster=F)
 plot <- plot + NoLegend()
-svg(str_replace(input_file, ".Rds", "_UMAP_label.svg"))
+svg(str_replace(dat, ".Rds", "_UMAP_label.svg"))
 print(plot)
 dev.off()
 
 
-saveRDS(pred, file = str_replace(input_file, ".Rds", "_singleR_results.Rds"))
-saveRDS(dat, file = str_replace(input_file, ".Rds", "_labelled.Rds"))
-write.table(df, file =str_replace(input_file, ".Rds", '_cell_labels.csv'), sep = '\t', row.names = T, col.names = T, quote = F)
+saveRDS(pred, file = str_replace(dat, ".Rds", "_singleR_results.Rds"))
+saveRDS(dat, file = str_replace(dat, ".Rds", "_labelled.Rds"))
+write.table(df, file =str_replace(dat, ".Rds", '_cell_labels.csv'), sep = '\t', row.names = T, col.names = T, quote = F)
