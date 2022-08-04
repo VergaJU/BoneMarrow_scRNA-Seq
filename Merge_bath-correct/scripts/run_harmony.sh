23ecef2577d13548b0aad18fac6678ff9767bae4
VergaJU <jacopoumberto.verga@gmail.com>
2022-08-04 17:55:17 +0100

    run combination of parameters for harmony

---
+++ Merge_bath-correct/scripts/run_harmony.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+theta=(1 2 3)
+lambda=(0.5 1 1.5)
+sigma=(0.05 0.1 0.15)
+
+
+for t in ${theta[@]};do 
+    for l in ${lambda[@]};do 
+        for s in ${sigma[@]};do 
+            ../scripts/run_harmony.R -i DATASET_seurat_PCA.Rds -t ${t} -l ${l} -s ${s} |& tee -a harmony.LOG
+        done
+    done
+done
