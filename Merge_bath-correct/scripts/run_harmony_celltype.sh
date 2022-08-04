df7e90fb1603454d43242ea269c2e3f0db464917
VergaJU <jacopoumberto.verga@gmail.com>
2022-08-04 18:39:16 +0100

    update log

--- Merge_bath-correct/scripts/run_harmony_celltype.sh
+++ Merge_bath-correct/scripts/run_harmony_celltype.sh
@@ -2,12 +2,14 @@
 
 export PATH="$PATH:/home/jacopo/Documents/GitHub/BoneMarrow_scRNA-Seq/Merge_bath-correct/scripts/"
 
+exec > >(tee -ia harmony.LOG)
+
 batch=(1 2 3)
 cell=(0 1 2)
 
 
 for b in ${batch[@]};do 
     for c in ${cell[@]};do 
-        run_harmony_celltype.R -i DATASET_seurat_PCA.Rds -t ${b} -l ${c} |& tee -a harmony.LOG
+        echo "run_harmony_celltype.R -i DATASET_seurat_PCA.Rds -t ${b} -l ${c} |& tee -a harmony.LOG"
     done
 done
