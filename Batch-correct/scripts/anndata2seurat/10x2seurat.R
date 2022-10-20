4afaf5bb190abd6011e3d216feae40d1a02793b4
VergaJU <jacopoumberto.verga@gmail.com>
2022-10-20 20:10:17 +0100

    created seurat2anndata

---
+++ Batch-correct/scripts/anndata2seurat/10x2seurat.R
@@ -0,0 +1,13 @@
+#!/home/jacopo/miniconda3/envs/seurat_env/bin/Rscript
+
+library(Seurat)
+
+dat <- Read10X("temp/")
+metadata <- read.csv("./temp/metadata.csv", row.names="X")
+dat <- CreateSeuratObject(dat, meta.data=metadata)
+umap <- read.csv("./temp/umap.csv", row.names="X")
+dat <- as.SingleCellExperiment(dat)
+reducedDim(dat, "umap") <- umap
+dat <- as.Seurat(dat)
+
+saveRDS(dat, "nk_raw_umap.Rds")
---
+++ Batch-correct/scripts/anndata2seurat/anndata210x.py
@@ -0,0 +1,18 @@
+#! /home/jacopo/miniconda3/envs/scanpy_env/bin/python
+import scipy.sparse as sparse
+import scipy.io as sio
+import scanpy as sc
+import pandas as pd
+
+
+adata = sc.read("nk_raw_umap.h5ad")
+
+mtx = adata.X.transpose()
+sio.mmwrite("./temp/matrix.mtx",mtx)
+barcodes = pd.DataFrame(adata.obs_names)
+barcodes.to_csv("./temp/barcodes.tsv", sep="\t", index=False, header=False)
+genes= pd.DataFrame(adata.var_names)
+genes[1] = genes[0]
+genes.to_csv("./temp/genes.tsv", sep="\t", index=False, header=False)
+adata.obs.to_csv("./temp/metadata.csv")
+pd.DataFrame(adata.obsm["X_umap"], columns = ["UMAP_1", "UMAP_2"], index=adata.obs_names).to_csv("./temp/umap.csv")
