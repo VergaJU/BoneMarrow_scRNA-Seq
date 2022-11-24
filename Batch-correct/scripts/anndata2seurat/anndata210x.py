30ab6889dba762de54cd1eae71f84ec5f6e940d0
VergaJU <jacopoumberto.verga@gmail.com>
2022-11-24 11:23:37 +0000

    update

--- Batch-correct/scripts/anndata2seurat/anndata210x.py
+++ Batch-correct/scripts/anndata2seurat/anndata210x.py
@@ -16,3 +16,7 @@ genes[1] = genes[0]
 genes.to_csv("./temp/genes.tsv", sep="\t", index=False, header=False)
 adata.obs.to_csv("./temp/metadata.csv")
 pd.DataFrame(adata.obsm["X_umap"], columns = ["UMAP_1", "UMAP_2"], index=adata.obs_names).to_csv("./temp/umap.csv")
+pd.DataFrame(adata.obsm["X_pca"], index=adata.obs_names).to_csv("./temp/pca.csv")
+hvg = adata.var.highly_variable
+hvg = pd.Series(hvg[hvg == True].index)
+hvg.to_csv("./temp/hvg.csv", index=None, header=None)
